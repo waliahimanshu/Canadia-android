bca8275c36af1275548e6ec21adeb163e0188127
Himanshu Walia <himanshu.walia@gmail.com>
2018-03-29 17:43:49 +0200

    connected with friebas db loaded data into recycler view adder side drawer to be used as filter

---
+++ app/src/main/java/com/waliahimanshu/canadia/ui/home/RoundOfInvitationModel.kt
@@ -0,0 +1,31 @@
+package com.waliahimanshu.canadia.ui.home
+
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.PropertyName
+import java.util.*
+
+
+@IgnoreExtraProperties
+class RoundOfInvitationModel {
+
+    @PropertyName("crs_draw_date")
+    lateinit var crsDrawDate: String
+
+    @PropertyName("crs_score")
+    lateinit var crsScore: String
+
+    @PropertyName("ita_issues")
+    lateinit var totalItaIssued: String
+
+    @PropertyName("tie_breaker_date")
+    lateinit var tieBreakerDate: String
+
+    constructor() {
+        // Default constructor required for calls to DataSnapshot.getValue(XX.class)
+    }
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    val ITEM_MAP: MutableMap<Int, RoundOfInvitationModel> = HashMap()
+}
